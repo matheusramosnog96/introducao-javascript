1680b693126a1ae48846f623347f5da977794f37
matheusramosnog96 <matheusramosnog96@gmail.com>
2020-10-05 23:11:50 -0300

    v2

--- js/principal.js
+++ js/principal.js
@@ -4,42 +4,49 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 
-var paciente = document.querySelector("#primeiro");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdImc = paciente.querySelector(".info-imc");//linha 30
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");//linha 30
 
 
-/*console.log(paciente);//tr
+    /*console.log(paciente);//tr
+    
+    console.log(tdPeso);//td que tem o peso
+    console.log(peso);
+    
+    console.log(tdAltura);
+    console.log(altura);
+    */
+    var pesoVlaido = true;
+    var alturaValida = true;
 
-console.log(tdPeso);//td que tem o peso
-console.log(peso);
+    if (peso <= 0 || peso >= 300) {
+        alert("peso inválido");
+        pesoVlaido = false;
+        tdImc.textContent = "Peso Inválido"
+        paciente.style.background = "lightcoral";
+    }
 
-console.log(tdAltura);
-console.log(altura);
-*/
-var pesoVlaido = true;
-var alturaValida = true;
+    if (altura <= 0 || altura >= 3.00) {
+        alert("altura inválida");
+        alturaValida = false;
+        tdImc.textContent = "Altura Inválido";
+        paciente.style.background = "lightcoral";
+    }
 
-if (peso <= 0 || peso >= 1000) {
-    alert("peso inválido");
-    pesoVlaido = false;
-    tdImc.textContent = "Peso Inválido"
+    if (alturaValida && pesoVlaido) {
+        var imc = peso / (altura * altura);
+        console.log(imc);
+        tdImc.textContent = imc.toFixed(2); //alterar no html
+    }
 }
-
-if (altura <= 0 || altura >= 3.00) {
-    alert("altura inválida");
-    alturaValida = false;
-    tdImc.textContent = "Altura Inválido";
-}
-
-if (alturaValida && pesoVlaido) {
-    var imc = peso / (altura * altura);
-    console.log(imc);
-    tdImc.textContent = imc; //alterar no html
-}
-
